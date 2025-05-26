1823990ff09fd7963cf1af669bdbf5c7534da5f7
Eric Hannafin <erichannafin1990@gmail.com>
2025-05-26 22:02:28 +0100

    Adding sqllite db connection

--- src/main/java/passwordmanager/App.java
+++ src/main/java/passwordmanager/App.java
@@ -1,11 +1,14 @@
 package passwordmanager;
 
+import passwordmanager.database.Database;
+
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-
+        Database.init();
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the password manager");
         System.out.println("*******************************");
@@ -14,4 +17,5 @@ public class App {
         System.out.println("Please enter your password");
         String password = sc.next();
     }
+
 }
---
+++ src/main/java/passwordmanager/database/Database.java
@@ -0,0 +1,42 @@
+package passwordmanager.database;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Database {
+
+    private static final String DB_URL = "jdbc:sqlite:passwords.db";
+
+    public static void init() {
+        System.out.println("Working directory: " + System.getProperty("user.dir"));
+        try(Connection connection = DriverManager.getConnection(DB_URL)){
+            System.out.println("Successfully connected to database");
+            Statement statement = connection.createStatement();
+            String sql = loadSqlFromResource("sql/init.sql");
+            statement.execute(sql);
+        } catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("Failed to establish database connection");
+        }
+    }
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(DB_URL);
+    }
+
+    public static String loadSqlFromResource(String filename) {
+        try (InputStream in = Database.class.getClassLoader().getResourceAsStream(filename);
+             Scanner scanner = new Scanner(in, StandardCharsets.UTF_8)) {
+            return scanner.useDelimiter("\\A").next();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load SQL from file: " + filename, e);
+        }
+    }
+
+}
