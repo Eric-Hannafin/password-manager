0b5bf57c8cfac8114ae7c30fd8a2c3831f6c3292
Eric Hannafin <erichannafin1990@gmail.com>
2025-05-28 21:39:28 +0100

    Creating AuthenticationService.java

--- src/main/java/passwordmanager/App.java
+++ src/main/java/passwordmanager/App.java
@@ -1,73 +1,21 @@
 package passwordmanager;
 
+import passwordmanager.authentication.AuthenticationService;
 import passwordmanager.database.Database;
-import passwordmanager.security.SecurityUtil;
 import passwordmanager.utility.MenuOption;
-
-import java.util.Scanner;
 
 public class App {
 
-    private static final Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
+        AuthenticationService authenticationService = new AuthenticationService();
         Database.init();
-        String initialResponse = initialDialogue();
+        String initialResponse = authenticationService.initialDialogue();
         MenuOption option = MenuOption.fromInput(initialResponse);
         switch (option){
-            case LOGIN -> login();
-            case REGISTER -> registerUser();
+            case LOGIN -> authenticationService.login();
+            case REGISTER -> authenticationService.registerUser();
             case EXIT -> System.out.println("Exiting....");
 
         }
-        sc.close();
-    }
-
-    private static String initialDialogue(){
-        System.out.println("************************************");
-        System.out.println("***Welcome to the PasswordManager***");
-        System.out.println("************************************");
-        System.out.println(" ");
-        System.out.println("Please select from the below options:");
-        System.out.println("1. Login");
-        System.out.println("2. Register");
-        System.out.println("3. Exit");
-
-        return sc.next();
-    }
-
-    private static void login() {
-    }
-
-    private static void registerUser() {
-        System.out.println(" ");
-        System.out.println("Please enter a username:");
-        String username = sc.next();  // Add check - username needs to be unique
-        String password = readPasswords();
-        SecurityUtil passwordUtility = new SecurityUtil();
-        byte[] salt = SecurityUtil.generateSalt();
-        Database.saveSalt(username, salt);
-        //SecurityUtil.generateKey(password, salt);
-    }
-
-    private static String readPasswords() {
-        String password1;
-        String password2;
-
-        while (true) {
-            System.out.println("Please enter a password:");
-            password1 = sc.next();
-
-            System.out.println("Please confirm your password:");
-            password2 = sc.next();
-
-            if (password1.equals(password2)) {
-                break;
-            } else {
-                System.out.println("Passwords do not match. Try again.");
-            }
-        }
-        System.out.println("Password accepted.");
-        return password1;
     }
 }
--- src/main/java/passwordmanager/authentication/AuthenticationService.java
+++ src/main/java/passwordmanager/authentication/AuthenticationService.java
@@ -1,4 +1,59 @@
 package passwordmanager.authentication;
 
+import passwordmanager.database.Database;
+import passwordmanager.security.SecurityUtil;
+
+import java.util.Scanner;
+
 public class AuthenticationService {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public String initialDialogue(){
+        System.out.println("************************************");
+        System.out.println("***Welcome to the PasswordManager***");
+        System.out.println("************************************");
+        System.out.println(" ");
+        System.out.println("Please select from the below options:");
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.println("3. Exit");
+
+        return sc.next();
+    }
+
+    public void login() {
+    }
+
+    public void registerUser() {
+        System.out.println(" ");
+        System.out.println("Please enter a username:");
+        String username = sc.next();  // Add check - username needs to be unique
+        String password = readPasswords();
+        SecurityUtil passwordUtility = new SecurityUtil();
+        byte[] salt = SecurityUtil.generateSalt();
+        Database.saveSalt(username, salt);
+        //SecurityUtil.generateKey(password, salt);
+    }
+
+    private String readPasswords() {
+        String password1;
+        String password2;
+
+        while (true) {
+            System.out.println("Please enter a password:");
+            password1 = sc.next();
+
+            System.out.println("Please confirm your password:");
+            password2 = sc.next();
+
+            if (password1.equals(password2)) {
+                break;
+            } else {
+                System.out.println("Passwords do not match. Try again.");
+            }
+        }
+        System.out.println("Password accepted.");
+        return password1;
+    }
 }
