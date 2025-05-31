05b0deb2b5c4f3a1a8361362a45bb57ffc83abb3
Eric Hannafin <erichannafin1990@gmail.com>
2025-05-31 13:33:37 +0100

    Adding MenuOptionEnumTest

---
+++ src/test/java/passwordmanager/security/utility/MenuOptionEnumTest.java
@@ -0,0 +1,27 @@
+package passwordmanager.security.utility;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import passwordmanager.utility.MenuOptionEnum;
+
+class MenuOptionEnumTest {
+
+    @Test
+    void returnsCorrectEnumForValidInput() {
+        Assertions.assertEquals(MenuOptionEnum.LOGIN, MenuOptionEnum.fromInput("1"));
+        Assertions.assertEquals(MenuOptionEnum.REGISTER, MenuOptionEnum.fromInput("2"));
+        Assertions.assertEquals(MenuOptionEnum.EXIT, MenuOptionEnum.fromInput("3"));
+    }
+
+    @Test
+    void returnsNullForInvalidInput() {
+        Assertions.assertNull(MenuOptionEnum.fromInput("4"));
+        Assertions.assertNull(MenuOptionEnum.fromInput("invalid"));
+        Assertions.assertNull(MenuOptionEnum.fromInput(""));
+    }
+
+    @Test
+    void handlesNullInputGracefully() {
+        Assertions.assertNull(MenuOptionEnum.fromInput(null));
+    }
+}
